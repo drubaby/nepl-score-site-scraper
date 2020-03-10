77b412060abcdf660f1b4127986040cf1ec78ec2
drubaby <peryton6@gmail.com>
2020-03-09 23:56:10 -0400

    write to json functionality

--- requestor.py
+++ requestor.py
@@ -2,53 +2,57 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-LiveURL = "http://nepl.league.papa.org/pointsByMeet/31"
+liveURL = "http://nepl.league.papa.org/pointsByMeet/31"
 localURL = "/Users/drubles/Development/code/score-scraper/pointsByMeet31.html"
 
 # r = requests.get(URL)
 # soup = BeautifulSoup(r.content)
 soup = BeautifulSoup(open(localURL), "html.parser")
 
-
+# Todo: remove this comment block used for writing liveUrl to local file
 # filename = "pointsByMeet31.html"
 # with open(filename, 'w') as f:
 #     f.write(str(soup))
 
+# pageDigest will collect all of the parsed page data into a dict with 3 keys
+# "seasonTitle", "accessLink", and "playerData"
+pageDigest = {}
 
-allPlayers = {}
+pageTitle = soup.find('title').text
+# Find just the relevant text to assign to seasonTitle Key
+seasonTitle = pageTitle.split(':')[0]
+pageDigest['seasonTitle'] = seasonTitle
+
+accessLink = soup.find('a', attrs = {'accesskey':'r'})
+pageDigest['accessLink'] = accessLink['href']
+
 scoreTable = soup.find('tbody')
 firstRow = scoreTable.find('tr')
 
+playerData = {}
 count = 0
-
 for row in scoreTable.findAll('tr'):
     player = {}
     nameCell = row.find('td')
     scoreCells = firstRow.findAll('td', attrs = {'class':'toggleStyle'})
     player['name'] = nameCell.text
-# #  build an array of player scores with 8 elements
+#  build an array of player scores with 8 elements
     scoreArray = []
     for td in scoreCells:
         scoreArray.append(td.text or "0")
     player['scores'] = scoreArray
-    allPlayers[count] = player
+    playerData[count] = player
     count += 1
-    print count
 
-# print "Players found: ", len(allPlayers)
-# print "first player: ", allPlayers[0]
+pageDigest['playerData'] = playerData
 
-print type(allPlayers)
+filename = seasonTitle.replace(" ", "_")
+result_json = json.dumps(playerData)
 
-# output of print(row) in for loop
-# <td>Aaron Rogers</td>
-# <td class="textCenter">100</td>
-# <td class="textCenter">100</td>
-# <td class="textCenter toggleStyle" data-style="" title=""></td>
-# <td class="textCenter toggleStyle" data-style="color:#6040A0" title="Group 8">10</td>
-# <td class="textCenter toggleStyle" data-style="color:#6040A0" title="Group 8">17</td>
-# <td class="textCenter toggleStyle" data-style="color:#B0B000" title="Group 5">32</td>
-# <td class="textCenter toggleStyle" data-style="" title=""></td>
-# <td class="textCenter toggleStyle" data-style="color:#009999" title="Group 11">22</td>
-# <td class="textCenter toggleStyle" data-style="color:#B0B000" title="Group 69">19</td>
-# <td class="textCenter toggleStyle" data-style="" title=""></td>
+# Write json to file named after season title
+with open('%s.json' % filename, 'w') as outfile:
+    json.dump(pageDigest, outfile)
+
+# Todo: also write result to a CSV file with a similar name
+#  Headers should be 'Player Name', 'Match 1', 'Match 2' etc depending on
+# number of matches in a playerScore
